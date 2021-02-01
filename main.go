259b1bf913ef69731980972a3d9ee43a42915343
nanandt <rizkyfatih007@gmail.com>
2021-02-01 22:22:32 +0700

    first commit and test connection to database

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/startup?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("connetion to database success")
+}
