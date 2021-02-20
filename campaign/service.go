f56d383cc15558f6424e9bb8238f2a8c181c61d1
nanandt <rizkyfatih007@gmail.com>
2021-02-21 02:23:57 +0700

    membua handler update campaign

--- campaign/service.go
+++ campaign/service.go
@@ -11,7 +11,7 @@ type Service interface {
 	GetCampaigns(userID int) ([]Campaign, error)
 	GetCampaignByID(input GetCampaignDetailInput) (Campaign, error)
 	CreateCampaign(input CreateCampaignInput) (Campaign, error)
-	Update(inputID GetCampaignDetailInput, inputData CreateCampaignInput)(Campaign, error)
+	UpdateCampaign(inputID GetCampaignDetailInput, inputData CreateCampaignInput)(Campaign, error)
 }
 
 type service struct {
@@ -68,7 +68,7 @@ func (s *service) CreateCampaign(input CreateCampaignInput) (Campaign, error) {
 	return newCampaign, nil
 }
 
-func (s *service) Update(inputID GetCampaignDetailInput, inputData CreateCampaignInput)(Campaign, error){
+func (s *service) UpdateCampaign(inputID GetCampaignDetailInput, inputData CreateCampaignInput)(Campaign, error){
 	campaign, err := s.repository.FindByID(inputID.ID)
 	if err != nil {
 		return campaign, err
