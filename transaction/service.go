f03ef70cc5bc789fc631ee831ae30e23b76d3706
nanandt <rizkyfatih007@gmail.com>
2021-02-22 16:10:28 +0700

    membuat service campaign transaction

--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,22 @@
 package transaction
+
+type service struct {
+	repository Repository
+}
+
+type Service interface {
+	GetTransactionsByCampaignID(input GetCampaignTrnsactionsInput) ([]Transaction, error)
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) GetTransactionsByCampaignID(input GetCampaignTrnsactionsInput) ([]Transaction, error) {
+	transactions, err := s.repository.GetByCampaignID(input.ID)
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
