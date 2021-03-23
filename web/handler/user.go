2c895443b50ca47cec337e0e70270779af9b226e
nanandt <rizkyfatih007@gmail.com>
2021-03-23 21:43:49 +0700

    form update user

--- web/handler/user.go
+++ web/handler/user.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"bwastartup/user"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -51,6 +52,17 @@ func (h *userHandler) Create(c *gin.Context){
 	}
 
 	c.Redirect(http.StatusFound,"/users")
+}
 
+func (h *userHandler) Edit(c *gin.Context){
+	idParam := c.Param("id")
+	id, _ := strconv.Atoi(idParam)
+
+	registerUser, err := h.userService.GetUserByID(id)
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+	}
+
+	c.HTML(http.StatusOK, "user_edit.html", registerUser)
 
 }
