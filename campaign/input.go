bce6d2de614d5b24b0029b1b8011aea0c47c77a2
nanandt <rizkyfatih007@gmail.com>
2021-03-30 23:27:25 +0700

    create campaign with cms

--- campaign/input.go
+++ campaign/input.go
@@ -17,6 +17,17 @@ type CreateCampaignInput struct {
 
 type CreateCampaignImageInput struct {
 	CampaignID int `form:"campaign_id" binding:"required"`
-	IsPrimary bool	`form:"is_primary"`
+	IsPrimary bool `form:"is_primary"`
 	User user.User
+}
+
+type FormCreateCampaignInput struct {
+	Name				string	`form:"name" binding:"required"`
+	ShortDescription	string	`form:"short_description" binding:"required"`
+	Description			string	`form:"description" binding:"required"`
+	GoalAmount			int		`form:"goal_amount" binding:"required"`
+	Perks				string	`form:"perks" binding:"required"`
+	UserID				int		`form:"user_id" binding:"required"`
+	Users				[]user.User
+	Error				error
 }
--- web/handler/campaign.go
+++ web/handler/campaign.go
@@ -2,16 +2,18 @@ package handler
 
 import (
 	"bwastartup/campaign"
+	"bwastartup/user"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 type campaignHandler struct{
 	campaignService campaign.Service
+	userService user.Service
 }
 
-func NewCampaignHandler(campaignService campaign.Service) *campaignHandler {
-	return &campaignHandler{campaignService}
+func NewCampaignHandler(campaignService campaign.Service, userService user.Service) *campaignHandler {
+	return &campaignHandler{campaignService, userService}
 }
 
 func (h *campaignHandler) Index(c *gin.Context)  {
@@ -22,4 +24,56 @@ func (h *campaignHandler) Index(c *gin.Context)  {
 	}
 
 	c.HTML(http.StatusOK, "campaign_index.html", gin.H{"campaigns":campaigns})
+}
+
+func(h *campaignHandler) New(c *gin.Context){
+	users, err := h.userService.GetAllUsers()
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		return
+	}
+
+	input := campaign.FormCreateCampaignInput{}
+	input.Users = users
+
+	c.HTML(http.StatusOK, "campaign_new.html", input)
+}
+
+func (h *campaignHandler) Create(c *gin.Context){
+	var input campaign.FormCreateCampaignInput
+	err := c.ShouldBind(&input)
+	if err != nil {
+		users, e := h.userService.GetAllUsers()
+		if e != nil {
+			c.HTML(http.StatusInternalServerError, "error.html", nil)
+			return
+		}
+		input.Users = users
+		input.Error = err
+
+		c.HTML(http.StatusOK, "campaign_new.html", input)
+		return
+	}
+
+	user, err := h.userService.GetUserByID(input.UserID)
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		return
+	}
+
+	createCampaignInput := campaign.CreateCampaignInput{}
+	createCampaignInput.Name = input.Name
+	createCampaignInput.ShortDescription = input.ShortDescription
+	createCampaignInput.Description = input.Description
+	createCampaignInput.GoalAmount = input.GoalAmount
+	createCampaignInput.Perks = input.Perks
+	createCampaignInput.User = user
+
+	_, err = h.campaignService.CreateCampaign(createCampaignInput)
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		return
+	}
+
+	c.Redirect(http.StatusFound, "/campaigns")
 }
